a84a7542131ddb5642fb26199e814d50c01ea9a1
kadenjones1212 <kadenjones1212@gmail.com>
2024-10-08 09:34:05 -0400

    Added comment for ppl to know where to make blog changes

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const body = document.body;
-    const maxWidth = window.innerWidth;
-    const maxHeight = document.documentElement.scrollHeight; // Use the full page height
-    const linePosX = 50;
-    const linePosY = 50;
-    // Create a container for the circles
-    const circleContainer = document.createElement("div");
-    circleContainer.classList.add("circle-container");
-    body.appendChild(circleContainer);
-
-    // Function to create a circle with custom criteria
-    function createCircle(color, xPercentage, yPercentage, size) {
-        const circle = document.createElement("div");
-        circle.classList.add("circle");
-        circle.style.backgroundColor = color;
-        circle.style.width = size + "px";
-        circle.style.height = size + "px";
-        circle.style.position = "fixed"; // Change position to fixed
-        circle.style.left = `calc(${xPercentage}% - ${size / 2}px)`;
-        circle.style.top = `calc(${yPercentage}% - ${size / 2}px)`;
-        circle.style.borderRadius = "50%";
-
-        circleContainer.appendChild(circle);
+<script>
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
     }
-
-    createCircle("#006", 99, 0, 500);
-    createCircle("whitesmoke", 100, 0, 499); 
-
-    createCircle("whitesmoke",100,100,195);
-    createCircle("#006", 0, 100, 500);
-    createCircle("whitesmoke", -1, 100, 499); 
-
-
-   
-
-
-
-    // Add more custom circles as needed using the createCircle function
-});
+  }
+}
+</script>
